767075047a2d6cf95ff1d31eadea00e8adae29f8
Tommy Lau <tlau425@yahoo.com>
2019-02-27 00:56:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
-def oxford_comma(array)
-if array.lenght == 1
-  
+def oxford_comma (array)
+if array.length == 1
+  array.join
 elsif array.length == 2
-    array.join(", and ")
+  array.join(", and ")
 elsif array.length > 2
 array[0..-2].join(", ") + ", and " + array[-1]
+end
 end
