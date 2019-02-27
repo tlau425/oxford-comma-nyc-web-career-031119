75d487a19e7086b60b5e08c5010f622767b5cf54
Tommy Lau <tlau425@yahoo.com>
2019-02-27 00:54:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 array.join
-elsif array
-
-elsif array.lenght > 2
+elsif array.length == 2
+    array.join(", and ")
+elsif array.length > 2
 array[0..-2].join(", ") + ", and " + array[-1]
 end
